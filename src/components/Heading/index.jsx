6300e3dbaad3faae948b85782370bd49058a08c3
João Vitor <joao_vitor.personal@Outlook.com>
2023-01-17 12:23:31 -0300

    feat: ensure <Heading /> renders with default color value

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -1,7 +1,13 @@
 import P from 'prop-types'
 const { Title } = require('./styles')
 
-const Heading = ({ children, appTheme, titleType, titleSize, upperCase }) => {
+const Heading = ({
+  children,
+  appTheme = 'LIGHT',
+  titleType = 'h1',
+  titleSize = 'medium',
+  upperCase = false,
+}) => {
   return (
     <header>
       <Title appTheme={appTheme} as={titleType} size={titleSize} uppercase={upperCase}>
@@ -13,9 +19,9 @@ const Heading = ({ children, appTheme, titleType, titleSize, upperCase }) => {
 
 Heading.propTypes = {
   children: P.node.isRequired,
-  appTheme: P.oneOf(['LIGHT', 'DARK']).isRequired,
-  titleType: P.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']).isRequired,
-  titleSize: P.oneOf(['small', 'medium', 'big']).isRequired,
+  appTheme: P.oneOf(['LIGHT', 'DARK']),
+  titleType: P.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
+  titleSize: P.oneOf(['small', 'medium', 'big']),
   upperCase: P.bool.isRequired,
 }
 
