f0d40b74b0bf5387f2a8b0b9751b6902a3fba822
João Vitor <joao_vitor.personal@Outlook.com>
2023-01-17 12:23:43 -0300

    test: ensure <Heading /> renders with default color value

---
+++ src/components/Heading/heading.test.js
@@ -0,0 +1,14 @@
+import { screen } from '@testing-library/react'
+import { Heading } from '.'
+import { renderTheme } from '../../styles/render-theme'
+import { theme } from '../../styles/theme'
+
+describe('<Heading />', () => {
+  test('should render heading with default color value', () => {
+    renderTheme(<Heading>text</Heading>)
+
+    const heading = screen.getByRole('heading', { name: 'text' })
+
+    expect(heading).toHaveStyleRule('color', theme.colors.primaryColor)
+  })
+})
