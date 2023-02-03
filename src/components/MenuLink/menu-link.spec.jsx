e2dc3bdb9a409509ba5f4a5764f9ffb73e5121bb
João Vitor <joao_vitor.personal@Outlook.com>
2023-02-03 19:42:53 -0300

    test: ensure <MenuLink> only renders with a text and link

--- src/components/MenuLink/menu-link.spec.jsx
+++ src/components/MenuLink/menu-link.spec.jsx
@@ -3,9 +3,10 @@ import { MenuLink } from '.'
 import { renderTheme } from '../../styles/render-theme'
 
 describe('<MenuLink />', () => {
-  test('should only render <MenuLink /> with a text', () => {
-    const { container } = renderTheme(<MenuLink link="any_link">any_text</MenuLink>)
+  test('should only render <MenuLink /> with a text and link', () => {
+    renderTheme(<MenuLink link="any_link">any_text</MenuLink>)
     const link = screen.queryByRole('link', { name: /any_text/i })
     expect(link).toBeInTheDocument()
+    expect(link).toHaveAttribute('href', 'any_link')
   })
 })
