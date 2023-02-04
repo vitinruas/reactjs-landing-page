c615f7774a6ccd9deae6d5966fd9808bb8afaba9
João Vitor <joao_vitor.personal@Outlook.com>
2023-02-04 00:43:59 -0300

    test: ensure <MenuLink> takes its snapshot

--- src/components/MenuLink/menu-link.spec.jsx
+++ src/components/MenuLink/menu-link.spec.jsx
@@ -5,8 +5,18 @@ import { renderTheme } from '../../styles/render-theme'
 describe('<MenuLink />', () => {
   test('should only render <MenuLink /> with a text and link', () => {
     renderTheme(<MenuLink link="any_link">any_text</MenuLink>)
+
     const link = screen.queryByRole('link', { name: /any_text/i })
+
     expect(link).toBeInTheDocument()
     expect(link).toHaveAttribute('href', 'any_link')
+  })
+
+  test('should take a snapshot', () => {
+    renderTheme(<MenuLink link="any_link">any_text</MenuLink>)
+
+    const link = screen.queryByRole('link', { name: /any_text/i })
+
+    expect(link).toMatchSnapshot()
   })
 })
